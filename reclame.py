17d09329b2f628789a34293696d359d06c913ed3
Michiel Lindenburg <michiellindenburg@hotmail.com>
2026-03-21 12:43:30 +0100

    huiswerk les 8

---
+++ reclame.py
@@ -0,0 +1,33 @@
+from algemene_functies import mijn_functie_2
+
+def aanbieding_1(smaak, prijs, korting):
+    prijs_na_korting = prijs * (1 - korting)
+    uitvoer = (
+        f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, "
+        f"van {prijs} euro voor {prijs_na_korting:.2f} euro."
+    )
+    return uitvoer
+
+def inkomsten_totaal (inkomsten, btw):
+    totaal = sum(inkomsten)
+    btw_bedrag = totaal * btw
+    uitvoer = (
+        f"Het totaal van alle inkomsten van deze week is {totaal} euro"
+        f"Waarover {btw_bedrag} euro btw betaald dient te worden"
+    )
+    return uitvoer
+
+def laag_en_hoog (mijn_lijst):
+    return [max(mijn_lijst), min(mijn_lijst)]
+
+def gemiddelde (mijn_lijst):
+    gem = sum(mijn_lijst) / len(mijn_lijst)
+    return f"De gemiddelde inkomsten deze week zijn {gem} euro."
+
+def meervoudig (invoer_lijst):
+    return laag_en_hoog (invoer_lijst)
+
+def combinatie (invoer_lijst_2):
+    korte_lijst = laag_en_hoog (invoer_lijst_2)
+    uitvoer = mijn_functie_2 (korte_lijst[0], korte_lijst[1])
+    return uitvoer
